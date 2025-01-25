cfa5a43bc0ffe7ede811fd1cfd0eb1cc00e0a16f
Martin Høy <martin@hoy.priv.no>
2025-01-25 19:36:24 +0100

    Update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,15 @@
 
 from __future__ import annotations
 
-import datetime
 import json
 import logging
 import pickle
 from pathlib import Path
+from typing import Any
 
 import pytest
 from flask import Flask
+from pydantic import Json
 
 import avgangstider
 from avgangstider.classes import Situation
@@ -17,32 +18,8 @@ from avgangstider.classes import Situation
 LOG = logging.getLogger(__name__)
 
 
-class FixedDateTime:
-    """A mock of datetime that provides a fixed value for now()."""
-
-    def __init__(self, timestamp: str | datetime.datetime) -> None:  # noqa: D107
-        if isinstance(timestamp, str):
-            self.timestamp = datetime.datetime.fromisoformat(timestamp)
-        elif isinstance(timestamp, datetime.datetime):
-            self.timestamp = timestamp
-
-    def now(self, tz: datetime.tzinfo | None = None) -> datetime.datetime:  # noqa: D102
-        if tz is None:
-            tz = self.timestamp.tzinfo
-        return self.timestamp.replace(tzinfo=tz)
-
-    @staticmethod
-    def fromisoformat(string: str) -> datetime.datetime:  # noqa: D102
-        return datetime.datetime.fromisoformat(string)
-
-
 @pytest.fixture
-def fixed_datetime() -> type[FixedDateTime]:  # noqa: D103
-    return FixedDateTime
-
-
-@pytest.fixture
-def saved_situations_json() -> dict[str, list[dict[str, str]]]:  # noqa: D103
+def saved_situations_json() -> Json[Any]:  # noqa: D103
     with Path(Path(__file__).parent / "data" / "situations.json").open() as file:
         return json.load(file)
 
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -2,34 +2,32 @@
 
 import datetime
 
+from freezegun import freeze_time
+
 import avgangstider.classes
 
-from .conftest import FixedDateTime
 
-
-def test_util(fixed_datetime: type[FixedDateTime]) -> None:  # noqa: D103
-    # Replace datetime in classes with a mocked class
-    mocked_now_string = "2019-01-01T12:00:00+01:00"
-    mocked_now = datetime.datetime.fromisoformat(mocked_now_string)
-    avgangstider.classes.datetime = fixed_datetime(mocked_now_string)  # type: ignore
-
+@freeze_time("2019-01-01T12:00:00+01:00")
+def test_util() -> None:
+    """Test the Departure class."""
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
     departure = avgangstider.classes.Departure(
-        line_id="",
+        line_id="RUT:Line:0",
         line_name="",
         destination="",
-        platform="",
-        departure_datetime=mocked_now,
-        bg_color="",
-        fg_color="",
+        platform="NSR:Quay:0",
+        departure_datetime=now,
+        bg_color="000000",
+        fg_color="FFFFFF",
     )
 
     # Check the departure string for different timedelta's
-    departure.departure_datetime = mocked_now + datetime.timedelta(seconds=59)
+    departure.departure_datetime = now + datetime.timedelta(seconds=59)
     assert departure.departure_string == "nå"
 
-    departure.departure_datetime = mocked_now + datetime.timedelta(minutes=30)
+    departure.departure_datetime = now + datetime.timedelta(minutes=30)
     assert departure.departure_string == "30 min"
 
-    departure.departure_datetime = mocked_now + datetime.timedelta(minutes=31)
+    departure.departure_datetime = now + datetime.timedelta(minutes=31)
     time_string = departure.departure_datetime.strftime("%H:%M")
     assert departure.departure_string == time_string
